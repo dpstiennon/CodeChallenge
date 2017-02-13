87095d98b9962c444a2336255fe4741e8d6d2949
David Stiennon <david.stiennon@gmail.com>
2017-02-13 16:28:04 -0600

    Cleanup of html

--- CodeChallenge/scripts/app.js
+++ CodeChallenge/scripts/app.js
@@ -9,16 +9,19 @@
 
             ctrl.formatData = function() {
                 return {
-                    name: ctrl.name
+                    name: ctrl.name,
                     weekday: ctrl.weekday
-                    
                 
                 };
             }
 
-            ctrl.submitFn = function() {
+            ctrl.submitFn = function(e) {
                 console.log(`${ctrl.weekday} && ${ctrl.name}`);
-                $http({ method: 'POST', url: '', data=});
+                e.preventDefault();
+                $http({ method: 'POST', url: '/api/SubmitChallenge', data: ctrl.formatData() })
+                    .success(function() {
+                        alert("it was sent!");
+                    });
             };
         });
 
