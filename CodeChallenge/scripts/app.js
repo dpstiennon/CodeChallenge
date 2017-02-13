f2a4bc827b91b67fcf177a33018976851c051e34
David Stiennon <david.stiennon@gmail.com>
2017-02-13 15:21:48 -0600

    Initial commit with angular controller

---
+++ CodeChallenge/scripts/app.js
@@ -0,0 +1,25 @@
+﻿(function() {
+
+    angular.module('challenge', [])
+        .controller('ChallengeController', function challengeCtrl($http) {
+            var ctrl = this;
+
+            ctrl.weekday = 'Mon';
+            ctrl.name = '';
+
+            ctrl.formatData = function() {
+                return {
+                    name: ctrl.name
+                    weekday: ctrl.weekday
+                    
+                
+                };
+            }
+
+            ctrl.submitFn = function() {
+                console.log(`${ctrl.weekday} && ${ctrl.name}`);
+                $http({ method: 'POST', url: '', data=});
+            };
+        });
+
+})();
